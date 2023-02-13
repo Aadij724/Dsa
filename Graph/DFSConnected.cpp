980ae2bfb12e4d2ae3a0cb1774ccdedd801d549d
Aadi Jain <aadij4945@gmail.com>
2023-02-13 11:58:40 +0530

    difush

--- Graph/DFSConnected.cpp
+++ Graph/DFSConnected.cpp
@@ -8,26 +8,21 @@ void addEdge( vector<int> adj[], int u, int v )
     adj[v].push_back(u);
 }
 
-void DFSRec( vector<int> adj[], int s, bool visited[] )
+void DFSRec( vector<int> adj[], int s, bool visited[])
 {
     visited[s]=true;
     cout<<s<<" ";
     for( int u: adj[s] )
         if( visited[u]==false )
-            DFSRec( adj, u, visited );
+            DFSRec(adj, u, visited );
 }
 
-void DFS( vector<int> adj[], int v )
+void DFS( vector<int> adj[], int v, int s)
 {
     bool visited[v];
     for( int i=0; i<v; i++ )
         visited[i]=false;
-    
-    for( int i=0; i<v; i++ )
-    {
-        if( visited[i]==false )
-            DFSRec( adj, i, visited );
-    }
+    DFSRec( adj, s, visited );
 }
 
 
@@ -36,10 +31,11 @@ int main()
     int v=5;
     vector<int> adj[v];
     addEdge( adj, 0, 1 );
-    addEdge( adj, 1, 2 );
     addEdge( adj, 0, 2 );
+    addEdge( adj, 1, 3 );
+    addEdge( adj, 2, 3 );
+    addEdge( adj, 1, 4 );
     addEdge( adj, 3, 4 );
-    
-    DFS( adj, v );
 
+    DFS( adj, v, 0);
 }
